4a70f89740510ceaa85589ce91281b0104384301
VinhHuuTran <tranhuuvinh1109@gmail.com>
2025-08-23 21:09:36 +0700

    test v5

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { usersProviders } from './users.providers';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, forwardRef(() => AuthModule)],
+  imports: [DatabaseModule],
   controllers: [UsersController],
   providers: [UsersService, ...usersProviders],
   exports: [UsersService],
