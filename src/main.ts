f646bbc01d7fe99774563060e8105a7e835e401e
VinhHuuTran <tranhuuvinh1109@gmail.com>
2025-08-30 10:58:57 +0700

    pass cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://your-domain.com'],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: false,
