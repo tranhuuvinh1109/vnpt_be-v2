8301504e61b61ada35f55ec9305a80b2111a54a7
VinhHuuTran <tranhuuvinh1109@gmail.com>
2025-08-30 17:25:52 +0700

    test cors

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-    credentials: false,
+    origin: ['https://YOUR-APP-NAME.vercel.app', 'http://localhost:3000'],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
   });
-
   const config = new DocumentBuilder()
     .setTitle('My API Docs')
     .setDescription('API documentation for my project')
